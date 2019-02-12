9c26c9be91586a44d033114cdba19c6e602b8761
zyucong <yingcongzhu@gmail.com>
2019-02-12 21:53:31 +1100

    roughly implement get /dummy/user

--- routes/dummy.js
+++ routes/dummy.js
@@ -1,10 +1,77 @@
 const express = require('express');
+const Joi = require('joi');
 const router = express.Router();
 
-router.get('/user', (req, res) => {
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/instacram', { useNewUrlParser: true })
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
+
+const User = mongoose.model('User', new mongoose.Schema({
+    id: { 
+        type: String
+    },
+    username: String,
+    email: String,
+    name: String,
+    following: String,
+    followed_num: {type: Number, min: 0}
+}));
+
+const Post = mongoose.model('Post', new mongoose.Schema({
+    id: String,
+    author: String,
+    comments: String,
+    description: String,
+    likes: String,
+    published: String,
+    src: String,
+    thumbnail: String
+}));
+
+router.get('/user', async (req, res) => {
     const username = req.query.username;
     const id = req.query.id;
-    console.log('get /dummy/user');
+    const {error} = validateUserId(req.query);
+    if (error) return res.status(400).send("Malformed Request");
+    if (username || id) {
+        const idcnt = await User.find({id: id}).countDocuments();
+        const usernamecnt = await User.find({username: username}).countDocuments();
+        if (idcnt === 0 && usernamecnt === 0) return res.status(404).send("User Not Found");
+        const idfunc = async function(){
+            if (idcnt) {
+                return id;
+            } else {
+                const user = await User.find({username: username});
+                return user[0].id;
+            }
+        }
+        const u_id = await idfunc();
+        let user = await User.find({id: u_id});
+        user = user[0];
+        const u_username = user.username;
+        const posts = await Post.find({author: u_username}).select({id: 1});
+        let post_array = [];
+        posts.forEach((e) => post_array.push(parseInt(e.id)));
+        const follow_list = string_to_set(user.following);
+        let follow_array = [];
+        follow_list.forEach(v => follow_array.push(parseInt(v)));
+        // [...follow_list]
+        console.log(user.followed_num);
+        res.send({
+            'username': u_username,
+            'name': user.name,
+            'id'  : parseInt(u_id),
+            'email': user.email,
+            // 'following': [...follow_list],
+            'following': follow_array,
+            // 'followed_num': parseInt(user.followed_num),
+            'followed_num': user.followed_num,
+            'posts':post_array
+        })
+    }
+    // console.log('get /dummy/user');
     res.send();
 });
 
@@ -19,8 +86,10 @@ router.put('/user', (req, res) => {
 router.get('/user/feed', (req, res) => {
     let p = req.query.p;
     let n = req.query.n;
-    if (typeof p == undefined || p == null) p = 0;
-    if (typeof n == undefined || n == null) n = 10;
+    // if (typeof p == undefined || p == null) p = 0;
+    // if (typeof n == undefined || n == null) n = 10;
+    if (!p) p = 0;
+    if (!n) n = 10;
     res.send([p, n]);
 });
 
@@ -72,5 +141,18 @@ router.put('/post/unlike', (req, res) => {
     const id = req.query.id;
     res.send();
 });
+
+function validateUserId(user) {
+    const schema = {
+        id: Joi.number().integer().min(0),
+        username: Joi.string()
+    }
+    return Joi.validate(user, schema);
+}
+
+function string_to_set(raw) {
+    if (!raw) return new Set([]);
+    return new Set(raw.split(','));
+}
 
 module.exports = router;
