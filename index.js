468d8f71cda67116f37a0949e774d9dc2daf578f
zyucong <yingcongzhu@gmail.com>
2019-02-17 21:00:19 +1100

    prepare to deploy

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const config = require('config');
 const dummy = require('./routes/dummy');
 const auth = require('./routes/auth');
 const user = require('./routes/user');
 
-mongoose.connect('mongodb://localhost/instacram', { useNewUrlParser: true })
+mongoose.connect(config.get('db'), { useNewUrlParser: true })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB', err));
 
